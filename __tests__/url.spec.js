23dc35d1f195b44bf82b5926880854c649ac747b
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-12 22:55:38 +0200

    use test files in js because it is much faster

---
+++ __tests__/url.spec.js
@@ -0,0 +1,42 @@
+require('./helper')
+const expect = require('expect')
+const { BaseHistory } = require('../src/history/base')
+
+const parseURL = BaseHistory.prototype.parseURL
+
+describe('URL parsing', () => {
+  it('works with no query no hash', () => {
+    expect(parseURL('/foo')).toEqual({
+      path: '/foo',
+      hash: '',
+      query: {},
+    })
+  })
+
+  it('extracts the query', () => {
+    expect(parseURL('/foo?a=one&b=two')).toEqual({
+      path: '/foo',
+      hash: '',
+      query: {
+        a: 'one',
+        b: 'two',
+      },
+    })
+  })
+
+  it('extracts the hash', () => {
+    expect(parseURL('/foo#bar')).toEqual({
+      path: '/foo',
+      hash: '#bar',
+      query: {},
+    })
+  })
+
+  it('extracts query and hash', () => {
+    expect(parseURL('/foo?a=one#bar')).toEqual({
+      path: '/foo',
+      hash: '#bar',
+      query: { a: 'one' },
+    })
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testMatch: [
-    '**/__tests__/**/*.spec.[jt]s?(x)',
-    '**/?(*.)+(spec|test).[jt]s?(x)',
+    '**/__tests__/**/*.spec.[j]s?(x)',
+    // '**/__tests__/**/*.spec.[jt]s?(x)',
+    // '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
   testEnvironment: 'node',
 }
