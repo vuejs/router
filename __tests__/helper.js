61b65e9a87c2d339457a578e82d0d1fb1ec50e78
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-16 17:57:13 +0200

    test: allow inline snapshot testing with mocha

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -1,4 +1,17 @@
 if (typeof global !== 'undefined' && typeof global.beforeAll === 'undefined') {
   global.beforeAll = global.before
   global.afterAll = global.after
+
+  const expect = require('expect')
+  // monkey patch jest snapshots
+  expect.extend({
+    toMatchInlineSnapshot(received, snapshot) {
+      const text = `[${received.toString()}]`
+      const pass = text === snapshot
+      return {
+        pass,
+        message: () => 'Snapshot not maching: ' + text,
+      }
+    },
+  })
 }
