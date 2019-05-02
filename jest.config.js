78a0a602b15729c3858aa7f23a2126975030efcb
Eduardo San Martin Morote <posva13@gmail.com>
2019-05-02 22:41:21 +0200

    test: ignore index.ts in coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/index.ts'],
   testMatch: [
     '**/__tests__/**/*.spec.[j]s?(x)',
     // '**/__tests__/**/*.spec.[jt]s?(x)',
