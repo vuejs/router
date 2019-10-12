47f06b1e5f7bb71bb4292f57ee9c3ae90a74751e
Eduardo San Martin Morote <posva13@gmail.com>
2019-10-12 17:02:18 +0200

    feat(hash): add wip hash history

---
+++ src/history/hash.2.ts
@@ -0,0 +1,8 @@
+import { RouterHistory } from './common'
+import createHistory from './html5.2'
+
+export default function createHashHistory(): RouterHistory {
+  // Make sure this implementation is fine in terms of encoding, specially for IE11
+  // @ts-ignore: TODO: implement it in history first
+  return createHistory('/#/')
+}
