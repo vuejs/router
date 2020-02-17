30737023d3fe2c990940a9196f4a0a601a417206
Eduardo San Martin Morote <posva13@gmail.com>
2020-02-17 21:16:21 +0100

    feat: export Link and View

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,18 @@
-import { createRouter, Router } from './router'
 import createHistory from './history/html5'
 import createMemoryHistory from './history/memory'
 import createHashHistory from './history/hash'
-import {
-  RouteLocationNormalized,
-  START_LOCATION_NORMALIZED as START_LOCATION,
-} from './types'
-import { onBeforeRouteLeave } from './navigationGuards'
-
-export { RouteLocationOptions } from './types'
-
-export { useRoute, useRouter } from './injectKeys'
 
 export {
-  createHistory,
-  createMemoryHistory,
-  createHashHistory,
-  createRouter,
   RouteLocationNormalized,
-  Router,
-  START_LOCATION,
-  onBeforeRouteLeave,
-}
+  START_LOCATION_NORMALIZED as START_LOCATION,
+  // needed for types, should probably be removed by changing the
+  RouteLocationOptions,
+} from './types'
+export { createRouter, Router } from './router'
+
+export { onBeforeRouteLeave } from './navigationGuards'
+export { useRoute, useRouter } from './injectKeys'
+export { Link } from './components/Link'
+export { View } from './components/View'
+
+export { createHistory, createMemoryHistory, createHashHistory }
