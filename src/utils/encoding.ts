90cb33d3cbe498a2a10eeff98102af0f798ca529
Ernest <iattempt.net@gmail.com>
2020-02-26 20:42:02 +0800

    refactor: make it more readable and self-explanatory.

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -17,53 +17,65 @@ import { warn } from 'vue'
 // const EXTRA_RESERVED_RE = /[!'()*]/g
 // const encodeReservedReplacer = (c: string) => '%' + c.charCodeAt(0).toString(16)
 
-const HASH_RE = /#/g // %23
-const AMPERSAND_RE = /&/g // %26
-const SLASH_RE = /\//g // %2F
-const EQUAL_RE = /=/g // %3D
-const IM_RE = /\?/g // %3F
+const RE_MAP = {
+  '#': /#/g,
+  '&': /&/g,
+  '/': /\//g,
+  '=': /=/g,
+  '?': /\?/g,
+}
 
-const ENC_BRACKET_OPEN_RE = /%5B/g // [
-const ENC_BRACKET_CLOSE_RE = /%5D/g // ]
-const ENC_CARET_RE = /%5E/g // ^
-const ENC_BACKTICK_RE = /%60/g // `
-const ENC_CURLY_OPEN_RE = /%7B/g // {
-const ENC_PIPE_RE = /%7C/g // |
-const ENC_CURLY_CLOSE_RE = /%7D/g // }
+const ENC_RE_MAP = {
+  '[': /%5B/g,
+  ']': /%5D/g,
+  '^': /%5E/g,
+  '`': /%60/g,
+  '{': /%7B/g,
+  '|': /%7C/g,
+  '}': /%7D/g,
+}
+const ENC_MAP = {
+  '#': '%23',
+  '&': '%26',
+  '/': '%2F',
+  '=': '%3D',
+  '?': '%3F',
+}
 
 function commonEncode(text: string | number): string {
   return encodeURI('' + text)
-    .replace(ENC_PIPE_RE, '|')
-    .replace(ENC_BRACKET_OPEN_RE, '[')
-    .replace(ENC_BRACKET_CLOSE_RE, ']')
+    .replace(ENC_RE_MAP['|'], '|')
+    .replace(ENC_RE_MAP['['], '[')
+    .replace(ENC_RE_MAP[']'], ']')
 }
 
 export function encodeHash(text: string): string {
   return commonEncode(text)
-    .replace(ENC_CURLY_OPEN_RE, '{')
-    .replace(ENC_CURLY_CLOSE_RE, '}')
-    .replace(ENC_CARET_RE, '^')
+    .replace(ENC_RE_MAP['{'], '{')
+    .replace(ENC_RE_MAP['}'], '}')
+    .replace(ENC_RE_MAP['^'], '^')
 }
 
 export function encodeQueryProperty(text: string | number): string {
   return commonEncode(text)
-    .replace(HASH_RE, '%23')
-    .replace(AMPERSAND_RE, '%26')
-    .replace(EQUAL_RE, '%3D')
-    .replace(ENC_BACKTICK_RE, '`')
-    .replace(ENC_CURLY_OPEN_RE, '{')
-    .replace(ENC_CURLY_CLOSE_RE, '}')
-    .replace(ENC_CARET_RE, '^')
+    .replace(RE_MAP['#'], ENC_MAP['#'])
+    .replace(RE_MAP['&'], ENC_MAP['&'])
+    .replace(RE_MAP['='], ENC_MAP['='])
+    .replace(ENC_RE_MAP['`'], '`')
+    .replace(ENC_RE_MAP['{'], '{')
+    .replace(ENC_RE_MAP['}'], '}')
+    .replace(ENC_RE_MAP['^'], '^')
 }
 
 export function encodePath(text: string): string {
   return commonEncode(text)
-    .replace(HASH_RE, '%23')
-    .replace(IM_RE, '%3F')
+    .replace(RE_MAP['#'], ENC_MAP['#'])
+    .replace(RE_MAP['?'], ENC_MAP['?'])
 }
 
 export function encodeParam(text: string): string {
-  return encodePath(text).replace(SLASH_RE, '%2F')
+  return encodePath(text)
+    .replace(RE_MAP['/'], ENC_MAP['/'])
 }
 
 export function decode(text: string): string {
