ffb13adb2081a1f5e96c42c4f0214035939f07d9
Eduardo San Martin Morote <posva13@gmail.com>
2020-03-24 11:29:22 +0100

    test: reactive unit test

--- __tests__/history/html5.spec.ts
+++ __tests__/history/html5.spec.ts
@@ -1,14 +1,17 @@
 import createWebHistory from '../../src/history/html5'
 import { createDom } from '../utils'
 
-// TODO: is it really worth testing this implementation on jest or is it
-// better to directly use e2e tests instead
-describe.skip('History HTMl5', () => {
+// These unit tests are supposed to tests very specific scenarios that are easier to setup
+// on a unit test than an e2e tests
+describe('History HTMl5', () => {
   beforeAll(() => {
     createDom()
   })
 
-  it('can be instantiated', () => {
+  // this problem is very common on hash history when using a regular link
+  // it will push an entry into the history stack with no state.
+  // When navigating back, we will try to read `state` but it will be null
+  it('should not fail if an entry has an empty state', () => {
     const history = createWebHistory()
     expect(history.location).toEqual({
       fullPath: '/',
