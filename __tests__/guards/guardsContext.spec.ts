ca754ff7f70dc1ff133b862c43493e73e1332793
Eduardo San Martin Morote <posva13@gmail.com>
2020-04-22 14:27:13 +0200

    test: refactor e2e as integration

--- __tests__/guards/guardsContext.spec.ts
+++ __tests__/guards/guardsContext.spec.ts
@@ -43,11 +43,190 @@ describe('beforeRouteLeave', () => {
 
     await router.isReady()
     await router.push('/leave')
-    await router.push('/').catch(() => {})
+    await router.push('/')
     expect(spy).toHaveBeenCalledTimes(1)
   })
 
-  it.todo('invokes with the component context with named views')
-  it.todo('invokes with the component context with nested views')
-  it.todo('invokes with the component context with nested named views')
+  it('invokes with the component context with named views', async () => {
+    expect.assertions(2)
+    const WithLeaveOne = defineComponent({
+      template: `text`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+    const WithLeaveTwo = defineComponent({
+      template: `text`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        { path: '/', component },
+        {
+          path: '/leave',
+          components: {
+            one: WithLeaveOne as any,
+            two: WithLeaveTwo as any,
+          },
+        },
+      ],
+    })
+    const app = createApp({
+      template: `
+      <router-view name="one" />
+      <router-view name="two" />
+      `,
+    })
+    app.use(router)
+    const rootEl = document.createElement('div')
+    document.body.appendChild(rootEl)
+    app.mount(rootEl)
+
+    await router.isReady()
+    await router.push('/leave')
+    await router.push('/')
+  })
+
+  it('invokes with the component context with nested views', async () => {
+    expect.assertions(2)
+    const WithLeaveParent = defineComponent({
+      template: `<router-view/>`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+    const WithLeave = defineComponent({
+      template: `text`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        { path: '/', component },
+        {
+          path: '/leave',
+          component: WithLeaveParent as any,
+          children: [
+            {
+              path: '',
+              component: WithLeave as any,
+            },
+          ],
+        },
+      ],
+    })
+    const app = createApp({
+      template: `
+      <router-view />
+      `,
+    })
+    app.use(router)
+    const rootEl = document.createElement('div')
+    document.body.appendChild(rootEl)
+    app.mount(rootEl)
+
+    await router.isReady()
+    await router.push('/leave')
+    await router.push('/')
+  })
+
+  it('invokes with the component context with nested named views', async () => {
+    expect.assertions(3)
+    const WithLeaveParent = defineComponent({
+      template: `
+      <router-view name="one"/>
+      <router-view name="two"/>
+      `,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+    const WithLeaveOne = defineComponent({
+      template: `text`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+    const WithLeaveTwo = defineComponent({
+      template: `text`,
+      // we use data to check if the context is the right one because saving `this` in a variable logs a few warnings
+      data: () => ({ counter: 0 }),
+      beforeRouteLeave: jest
+        .fn()
+        .mockImplementationOnce(function(this: any, to, from, next) {
+          expect(typeof this.counter).toBe('number')
+          next()
+        }),
+    })
+
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        { path: '/', component },
+        {
+          path: '/leave',
+          component: WithLeaveParent as any,
+          children: [
+            {
+              path: '',
+              components: {
+                one: WithLeaveOne as any,
+                two: WithLeaveTwo as any,
+              },
+            },
+          ],
+        },
+      ],
+    })
+    const app = createApp({
+      template: `
+      <router-view />
+      `,
+    })
+    app.use(router)
+    const rootEl = document.createElement('div')
+    document.body.appendChild(rootEl)
+    app.mount(rootEl)
+
+    await router.isReady()
+    await router.push('/leave')
+    await router.push('/')
+  })
 })
