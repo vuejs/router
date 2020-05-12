5031dc29b641083cb2a331746ea0a171f5b4840c
Eduardo San Martin Morote <posva13@gmail.com>
2020-05-12 16:26:02 +0200

    test: add basic ssr test

---
+++ __tests__/ssr.spec.ts
@@ -0,0 +1,58 @@
+/**
+ * @jest-environment node
+ */
+import { createRouter, createMemoryHistory } from '../src'
+import { createSSRApp, resolveComponent, Component } from 'vue'
+import {
+  renderToString,
+  ssrInterpolate,
+  ssrRenderComponent,
+} from '@vue/server-renderer'
+
+describe('SSR', () => {
+  it('works', async () => {
+    const Home = {
+      ssrRender(ctx: any, push: any) {
+        push('Home')
+      },
+    }
+    const Page = {
+      ssrRender(ctx: any, push: any) {
+        push(`${ssrInterpolate(ctx.$route.fullPath)}`)
+      },
+    }
+
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        { path: '/', component: Home },
+        {
+          path: '/:id',
+          component: Page,
+        },
+      ],
+    })
+    const App = {
+      ssrRender(ctx: any, push: any, parent: any) {
+        push(
+          ssrRenderComponent(
+            resolveComponent('router-view') as Component,
+            null,
+            null,
+            parent
+          )
+        )
+      },
+    }
+    const app = createSSRApp(App)
+    app.use(router)
+    // const rootEl = document.createElement('div')
+    // document.body.appendChild(rootEl)
+
+    router.push('/hello')
+    await router.isReady()
+
+    const xxx = await renderToString(app)
+    expect(xxx).toMatchInlineSnapshot(`"/hello"`)
+  })
+})
