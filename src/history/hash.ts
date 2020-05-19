fc7b9eca189f28cce5f7782592f3b130b5a8ac26
Eduardo San Martin Morote <posva13@gmail.com>
2020-05-19 14:24:09 +0200

    docs: add missing hyphen
    [skip ci]

--- src/history/hash.ts
+++ src/history/hash.ts
@@ -4,7 +4,7 @@ import { createWebHistory } from './html5'
 /**
  * Creates a hash history.
  *
- * @param base optional base to provide. Defaults to `/`
+ * @param base - optional base to provide. Defaults to `/`
  */
 export function createWebHashHistory(base: string = '/'): RouterHistory {
   // Make sure this implementation is fine in terms of encoding, specially for IE11
