8080eabd272ddfe072daa217186fd0664a4f38a6
Eduardo San Martin Morote <posva13@gmail.com>
2020-05-19 12:01:00 +0200

    docs: doc createWebHashHistory

--- src/history/hash.ts
+++ src/history/hash.ts
@@ -1,6 +1,11 @@
 import { RouterHistory } from './common'
 import { createWebHistory } from './html5'
 
+/**
+ * Creates a hash history.
+ *
+ * @param base optional base to provide. Defaults to `/`
+ */
 export function createWebHashHistory(base: string = '/'): RouterHistory {
   // Make sure this implementation is fine in terms of encoding, specially for IE11
   return createWebHistory(location.host ? base + '#' : '#')
