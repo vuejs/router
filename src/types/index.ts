ab7e652ff29207fd33520ac8215938ae581a12f3
Eduardo San Martin Morote <posva@users.noreply.github.com>
2020-06-23 14:54:29 +0200

    Update src/types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,6 @@
 import { LocationQuery, LocationQueryRaw } from '../query'
 import { PathParserOptions } from '../matcher'
-import {
-  Ref,
-  ComputedRef,
-  Component,
-  ComponentPublicInstance,
-} from 'vue'
+import { Ref, ComputedRef, Component, ComponentPublicInstance } from 'vue'
 import { RouteRecord, RouteRecordNormalized } from '../matcher/types'
 import { HistoryState } from '../history/common'
 import { NavigationFailure } from '../errors'
