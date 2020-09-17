b61be6eec1132e2929341271a2e07917c2f105b4
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-17 21:17:35 +0200

    docs: add theme for ads for next vitepress

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,36 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top>
+      <CarbonAds
+        v-if="$site.themeConfig.carbonAds"
+        :key="$page.path"
+        :code="$site.themeConfig.carbonAds.carbon"
+        :placement="$site.themeConfig.carbonAds.placement"
+      />
+    </template>
+    <template #page-bottom>
+      <BuySellAds
+        v-if="$site.themeConfig.carbonAds"
+        :key="$page.path"
+        :code="$site.themeConfig.carbonAds.custom"
+        :placement="$site.themeConfig.carbonAds.placement"
+      />
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import DefaultTheme from 'vitepress/dist/client/theme-default'
+import CarbonAds from './components/CarbonAds.vue'
+import BuySellAds from './components/BuySellAds.vue'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    ParentLayout: DefaultTheme.Layout,
+    CarbonAds,
+    BuySellAds,
+  },
+}
+</script>
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,12 @@
+import DefaultTheme from 'vitepress/dist/client/theme-default'
+import Layout from './Layout.vue'
+
+export default {
+  ...DefaultTheme,
+  Layout,
+  enhanceApp({ app, router, siteData }) {
+    // app is the Vue 3 app instance from createApp()
+    // router is VitePress' custom router (see `lib/app/router.js`)
+    // siteData is a ref of current site-level metadata.
+  },
+}
