540fca45ed7a22d79ac32849cfa78f9d0528da7b
Eduardo San Martin Morote <posva13@gmail.com>
2020-09-18 00:00:28 +0200

    docs: upgrade vitepress

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,6 +5,7 @@ export default {
   ...DefaultTheme,
   Layout,
   enhanceApp({ app, router, siteData }) {
+    // test
     // app is the Vue 3 app instance from createApp()
     // router is VitePress' custom router (see `lib/app/router.js`)
     // siteData is a ref of current site-level metadata.
