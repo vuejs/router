bd84d0e68cf0a981fd02d201bd93397f8b695cd1
Eduardo San Martin Morote <posva13@gmail.com>
2021-01-04 11:01:13 +0100

    docs: note the importance of base in hash mode
    Close #685

--- src/history/hash.ts
+++ src/history/hash.ts
@@ -4,11 +4,14 @@ import { warn } from '../warning'
 
 /**
  * Creates a hash history. Useful for web applications with no host (e.g.
- * `file://`) or when configuring a server to handle any URL.
+ * `file://`) or when configuring a server to handle any URL is not possible.
  *
  * @param base - optional base to provide. Defaults to `location.pathname` or
- * `/` if at root. If there is a `base` tag in the `head`, its value will be
- * **ignored**.
+ * `/` if at root. If there is a `<base>` tag in the `head`, its value will be
+ * ignored in favor of this parameter **but note it affects all the
+ * history.pushState() calls**, meaning that if you use a `<base>` tag, it's
+ * `href` value **has to match this parameter** (ignoring anything after the
+ * `#`).
  *
  * @example
  * ```js
