ced02429e92b4313140f46e8c2eba15e4180a73f
Nico Devs <3766839+nicodevs@users.noreply.github.com>
2021-06-09 12:07:37 -0300

    docs: Add VueSchool Summer Sale banner (#991)
    * docs(docs): Add VueSchool Summer Sale banner
    
    * docs(docs): Update VueSchool Summer Sale banner
    
    * docs(docs): Make BannerTop async

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,42 +1,65 @@
 <template>
-  <ParentLayout>
-    <template #sidebar-bottom>
-      <div class="sponsors">
-        <a
-          href="https://github.com/sponsors/posva"
-          target="_blank"
-          rel="noopener"
-          >Sponsors</a
-        >
+  <div
+    class="main-container"
+    :class="{ 'has-top-banner': showTopBanner }"
+  >
+    <BannerTop
+      v-if="showTopBanner"
+      @close="closeBannerTop"
+    />
+    <ParentLayout>
+      <template #sidebar-bottom>
+        <div class="sponsors">
+          <a
+            href="https://github.com/sponsors/posva"
+            target="_blank"
+            rel="noopener"
+            >Sponsors</a
+          >
 
-        <a
-          v-for="sponsor in sponsors.gold"
-          :href="sponsor.href"
-          :key="sponsor.href"
-          target="_blank"
-          rel="noopener"
-        >
-          <img :src="sponsor.imgSrcLight" :alt="sponsor.alt" />
-        </a>
-      </div>
-    </template>
-  </ParentLayout>
+          <a
+            v-for="sponsor in sponsors.gold"
+            :href="sponsor.href"
+            :key="sponsor.href"
+            target="_blank"
+            rel="noopener"
+          >
+            <img :src="sponsor.imgSrcLight" :alt="sponsor.alt" />
+          </a>
+        </div>
+      </template>
+    </ParentLayout>
+  </div>
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 import DefaultTheme from 'vitepress/dist/client/theme-default'
 import sponsors from '../components/sponsors.json'
 
+const BannerTop = defineAsyncComponent(() => import('../components/BannerTop.vue'))
+
 export default {
   name: 'Layout',
-
   components: {
     ParentLayout: DefaultTheme.Layout,
+    BannerTop
   },
-
-  setup() {
-    return { sponsors }
+  data () {
+    return {
+      sponsors,
+      showTopBanner: false
+    }
   },
+  mounted () {
+    this.showTopBanner = !localStorage.getItem('VS_SUMMER_BANNER_CLOSED')
+  },
+  methods: {
+    closeBannerTop () {
+      this.showTopBanner = false
+      localStorage.setItem('VS_SUMMER_BANNER_CLOSED', 1)
+    }
+  }
 }
 </script>
 
