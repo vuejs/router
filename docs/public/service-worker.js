4f6056533eb58ef09259c25b1dc85514a9b9c07e
Evan You <yyx990803@gmail.com>
2022-02-07 14:00:49 +0800

    docs: force clear service worker

---
+++ docs/public/service-worker.js
@@ -0,0 +1,15 @@
+// force clearing previous service worker
+self.addEventListener('install', function (e) {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll()
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url))
+    })
+})
