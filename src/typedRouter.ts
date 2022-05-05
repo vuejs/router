4da50f4d36184c065fba6fcbb0b1a96c6c7ca3e3
Eduardo San Martin Morote <posva13@gmail.com>
2022-05-05 14:31:23 +0200

    test: make them work

--- src/typedRouter.ts
+++ src/typedRouter.ts
@@ -1,5 +1,26 @@
-import { Router } from './router'
+import type { Router } from './router'
 
+/**
+ * Vue Router Configuration that allows to add global types for better type support.
+ *
+ * @example
+ *
+ * ```ts
+ * const router = createRouter({
+ *   // ...
+ *   routes: [
+ *     // ...
+ *   ] as const // IMPORTANT
+ * })
+ *
+ * declare module 'vue-router' {
+ *   export interface Config {
+ *     // allow global functions to get a typed router
+ *     Router: typeof router
+ *   }
+ * }
+ * ```
+ */
 export interface Config {
   // Router: unknown
 }
