4603d1d26125dace5d2b25572d7c3070476cecf6
Eduardo San Martin Morote <posva13@gmail.com>
2022-06-08 16:24:08 +0200

    refactor: move docs to package

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -3,4 +3,11 @@
 # check if doc files changes for netlify
 # needed because we cannot use && in netlify.toml
 
-git diff --quiet 'HEAD^' HEAD ./docs/ && ! git diff 'HEAD^' HEAD ./yarn.lock | grep --quiet vite
+# exit 0 will skip the build while exit 1 will build
+
+# - check any change in docs
+# - check for new version of vite related deps
+# - changes in netlify conf
+# - a commit message that starts with docs like docs: ... or docs(nuxt): ...
+
+git diff --quiet 'HEAD^' HEAD ./packages/docs/ && ! git diff 'HEAD^' HEAD ./pnpm-lock.yaml | grep --quiet vitepress && git diff --quiet 'HEAD^' HEAD netlify.toml && ! git log -1 --pretty=%B | grep '^docs'
