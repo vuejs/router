31aaf3b82aae5c553a0bacd52d0be1c602e0ab30
Eduardo San Martin Morote <posva13@gmail.com>
2022-06-29 18:18:08 +0200

    feat(types): allow extending global types

---
+++ packages/router/src/config.ts
@@ -0,0 +1,6 @@
+/**
+ * Allows customizing existing types of the router that are used globally like `$router`, `<RouterLink>`, and `beforeRouteLeave()`. **ONLY FOR INTERNAL USAGE**.
+ *
+ * @internal
+ */
+export interface TypesConfig {}
