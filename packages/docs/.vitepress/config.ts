18c5977051942d1a1903d4bbff6cd555902b0b2a
Suyan <suyanhanx@gmail.com>
2022-11-03 19:55:16 +0800

    docs: replace zh docs edit link text in chinese (#1594)
    Co-authored-by: Eduardo San Martin Morote <posva@users.noreply.github.com>

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -200,6 +200,7 @@ const config = defineConfig({
       '/zh/': {
         label: '中文',
         selectText: '选择语言',
+        editLinkText: '为此页提供修改建议',
         nav: [
           {
             text: '教程',
