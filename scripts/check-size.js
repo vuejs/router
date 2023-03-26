2c70c3aa74d3df488307c521da5ec850873e61db
Kirk Lin <linkirk@163.com>
2023-03-26 11:49:05 +0800

    perf: check-size.js

--- scripts/check-size.js
+++ scripts/check-size.js
@@ -1,29 +1,38 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const chalk = require('chalk')
-const { gzipSync } = require('zlib')
+const { gzip } = require('zlib')
 const { compress } = require('brotli')
 
-function checkFileSize(filePath) {
-  if (!fs.existsSync(filePath)) {
-    return
+async function checkFileSize(filePath) {
+  try {
+    const file = await fs.readFile(filePath)
+    const minSize = (file.length / 1024).toFixed(2) + 'kb'
+    const [gzipped, compressed] = await Promise.all([
+      gzip(file),
+      compress(file),
+    ])
+    const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
+    const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
+    console.log(
+      `${chalk.gray(
+        chalk.bold(path.basename(filePath))
+      )} min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
+    )
+  } catch (err) {
+    console.error(err)
   }
-  const file = fs.readFileSync(filePath)
-  const minSize = (file.length / 1024).toFixed(2) + 'kb'
-  const gzipped = gzipSync(file)
-  const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
-  const compressed = compress(file)
-  const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
-  console.log(
-    `${chalk.gray(
-      chalk.bold(path.basename(filePath))
-    )} min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
-  )
 }
 
-checkFileSize(
-  path.resolve(__dirname, '../packages/router/size-checks/dist/webRouter.js')
-)
-checkFileSize(
-  path.resolve(__dirname, '../packages/router/dist/vue-router.global.prod.js')
-)
+;(async () => {
+  const files = [
+    path.resolve(__dirname, '../packages/router/size-checks/dist/webRouter.js'),
+    path.resolve(
+      __dirname,
+      '../packages/router/dist/vue-router.global.prod.js'
+    ),
+  ]
+  for (const file of files) {
+    await checkFileSize(file)
+  }
+})()
