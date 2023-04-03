28e85d09288581a44482af0f0d5f4f374e5aa6ef
Kirk Lin <linkirk@163.com>
2023-04-03 21:01:58 +0800

    fix: remove the try/catch

--- scripts/check-size.js
+++ scripts/check-size.js
@@ -5,23 +5,16 @@ const { gzip } = require('zlib')
 const { compress } = require('brotli')
 
 async function checkFileSize(filePath) {
-  try {
-    const file = await fs.readFile(filePath)
-    const minSize = (file.length / 1024).toFixed(2) + 'kb'
-    const [gzipped, compressed] = await Promise.all([
-      gzip(file),
-      compress(file),
-    ])
-    const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
-    const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
-    console.log(
-      `${chalk.gray(
-        chalk.bold(path.basename(filePath))
-      )} min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
-    )
-  } catch (err) {
-    console.error(err)
-  }
+  const file = await fs.readFile(filePath)
+  const minSize = (file.length / 1024).toFixed(2) + 'kb'
+  const [gzipped, compressed] = await Promise.all([gzip(file), compress(file)])
+  const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
+  const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
+  console.log(
+    `${chalk.gray(
+      chalk.bold(path.basename(filePath))
+    )} min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
+  )
 }
 
 ;(async () => {
