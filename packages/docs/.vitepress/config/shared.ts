7d992a2d8543bf8470eed79cabc87e89f9ae5770
Eduardo San Martin Morote <posva13@gmail.com>
2023-08-01 20:16:11 +0200

    docs: upgrade api docs generation
    No longer need to manually edit duplicated links

--- packages/docs/.vitepress/config/shared.ts
+++ packages/docs/.vitepress/config/shared.ts
@@ -35,6 +35,10 @@ export const sharedConfig = defineConfig({
       leftDelimiter: '%{',
       rightDelimiter: '}%',
     },
+
+    anchor: {
+      slugify: s => s.replace(/\s/g, '-'),
+    },
   },
 
   head: [
--- packages/docs/typedoc-markdown.js
+++ packages/docs/typedoc-markdown.js
@@ -2,9 +2,6 @@ const _fs = require('fs')
 const path = require('path')
 const TypeDoc = require('typedoc')
 const { PageEvent } = TypeDoc
-const {
-  prependYAML,
-} = require('typedoc-plugin-markdown/dist/utils/front-matter')
 
 const fs = _fs.promises
 
@@ -15,6 +12,7 @@ const DEFAULT_OPTIONS = {
   readme: 'none',
   out: path.resolve(__dirname, './api'),
   entryDocument: 'index.md',
+  preserveAnchorCasing: true,
   hideBreadcrumbs: false,
   hideInPageTOC: true,
 }
@@ -33,13 +31,6 @@ exports.createTypeDocApp = function createTypeDocApp(config = {}) {
 
   // If you want TypeDoc to load tsconfig.json / typedoc.json files
   app.options.addReader(new TypeDoc.TSConfigReader())
-  // app.options.addReader(new TypeDoc.TypeDocReader())
-
-  /** @type {'build' | 'serve'} */
-  let targetMode = 'build'
-
-  const slugify = s => s.replaceAll(' ', '-')
-  // encodeURIComponent(String(s).trim().toLowerCase().replace(/\s+/g, '-'))
 
   app.renderer.on(
     PageEvent.END,
@@ -48,55 +39,10 @@ exports.createTypeDocApp = function createTypeDocApp(config = {}) {
      * @param {import('typedoc/dist/lib/output/events').PageEvent} page
      */
     page => {
-      if (page.url !== 'index.md' && page.contents) {
-        page.contents = prependYAML(page.contents, {
-          // TODO: figure out a way to point to the source files?
-          editLink: false,
-        })
-      }
-
-      // avoid duplicated id titles
-      if (page.contents) {
-        const lines = page.contents.split('\n')
-        const titleStack = []
-        let currentLevel = 0
-        const TITLE_LEVEL = /^#+/
-        const existingIds = new Map()
-        for (let i = 0; i < lines.length; i++) {
-          const line = lines[i]
-          if (!line.startsWith('#')) continue
-          const level = line.match(TITLE_LEVEL)[0].length
-
-          // remove extra levels
-          if (level <= currentLevel) {
-            titleStack.splice(level - 1)
-          }
-          // add the current title
-          titleStack.push(line.slice(level).trim())
-          currentLevel = level
-
-          // no need to add ids to h1
-          if (level < 2) continue
-
-          // ignore the root level (h1) to match the sidebar
-          const slugifiedTitle = slugify(titleStack.slice(1).join('-'))
-            // ensure the link is valid #1743
-            .replaceAll('\\', '')
-          let id
-          if (existingIds.has(slugifiedTitle)) {
-            const current = existingIds.get(slugifiedTitle)
-            existingIds.set(slugifiedTitle, current + 1)
-            id = ` %{#${slugifiedTitle}_${current + 1}}%`
-          } else {
-            existingIds.set(slugifiedTitle, 0)
-            id = ` %{#${slugifiedTitle}}%`
-          }
-          const newLine = line + id
-          lines.splice(i, 1, newLine)
-        }
-
-        page.contents = lines.join('\n')
-      }
+      page.contents = prependYAML(page.contents, {
+        // TODO: figure out a way to point to the source files?
+        editLink: false,
+      })
     }
   )
 
@@ -155,4 +101,44 @@ async function exists(path) {
   } catch {
     return false
   }
+}
+/**
+ * @typedef {Record<string, string | number | boolean>} FrontMatterVars
+ */
+
+/**
+ * Prepends YAML block to a string
+ * @param {string} contents - string to prepend to
+ * @param {FrontMatterVars} vars - object of required front matter variables
+ */
+function prependYAML(contents, vars) {
+  return contents
+    .replace(/^/, toYAML(vars) + '\n\n')
+    .replace(/[\r\n]{3,}/g, '\n\n')
+}
+
+/**
+ * Converts YAML object to a YAML string
+ * @param {FrontMatterVars} vars
+ */
+function toYAML(vars) {
+  const yaml = `---
+${Object.entries(vars)
+  .map(
+    ([key, value]) =>
+      `${key}: ${
+        typeof value === 'string' ? `"${escapeDoubleQuotes(value)}"` : value
+      }`
+  )
+  .join('\n')}
+---`
+  return yaml
+}
+
+/**
+ * Escapes double quotes in a string
+ * @param {string} str - string to escape
+ */
+function escapeDoubleQuotes(str) {
+  return str.replace(/"/g, '\\"')
 }
