ad65dc2fd4c01c7bcf6a222f5609926f6ef6606c
Jinjiang <zhaojinjiang@me.com>
2024-01-29 17:43:33 +0800

    refactor: replace translation scripts and ui with vitepress-translation-helper (#2109)
    * refactor: replace translation scripts and ui with vitepress-translation-helper
    
    * chore: remove old translation scripts
    
    * chore: bump vitepress-translation-helper
    
    * chore: bump vitepress-translation-helper
    
    * Update index.ts
    
    ---------
    
    Co-authored-by: Eduardo San Martin Morote <posva@users.noreply.github.com>

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,11 +3,16 @@ import { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import AsideSponsors from './components/AsideSponsors.vue'
 // import HomeSponsors from './components/HomeSponsors.vue'
-import TranslationStatus from './components/TranslationStatus.vue'
+import TranslationStatus from 'vitepress-translation-helper/ui/TranslationStatus.vue'
 import './styles/vars.css'
 import './styles/sponsors.css'
 import VueSchoolLink from './components/VueSchoolLink.vue'
 import VueMasteryLogoLink from './components/VueMasteryLogoLink.vue'
+import status from '../translation-status.json'
+
+const i18nLabels = {
+  zh: '该翻译已同步到了 ${date} 的版本，其对应的 commit hash 是 <code>${hash}</code>。',
+}
 
 const theme: Theme = {
   ...DefaultTheme,
@@ -15,7 +20,7 @@ const theme: Theme = {
     return h(DefaultTheme.Layout, null, {
       // 'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors),
-      'doc-before': () => h(TranslationStatus),
+      'doc-before': () => h(TranslationStatus, { status, i18nLabels }),
     })
   },
 
