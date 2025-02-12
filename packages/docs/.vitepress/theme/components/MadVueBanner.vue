fb6e90c206fcfa20bd2d1e96b4535e2ca3a75587
Israel Ortuño <ai.ortuno@gmail.com>
2025-02-12 11:53:20 +0100

    chore: action text

--- packages/docs/.vitepress/theme/components/MadVueBanner.vue
+++ packages/docs/.vitepress/theme/components/MadVueBanner.vue
@@ -76,7 +76,7 @@
 
     </div>
     <a target="_blank" class="action" :href="target">
-      Know more
+      More Info
       <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" style="margin-left: 10px;" viewBox="0 0 15 15">
         <path fill="currentColor"
           d="M8.293 2.293a1 1 0 0 1 1.414 0l4.5 4.5a1 1 0 0 1 0 1.414l-4.5 4.5a1 1 0 0 1-1.414-1.414L11 8.5H1.5a1 1 0 0 1 0-2H11L8.293 3.707a1 1 0 0 1 0-1.414" />
