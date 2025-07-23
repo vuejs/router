447747128674dffbd8b3ba894f2aa2a3dab13d9a
Eduardo San Martin Morote <posva13@gmail.com>
2025-07-23 23:25:10 +0200

    chore: show route info in playground

--- packages/experiments-playground/src/App.vue
+++ packages/experiments-playground/src/App.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <template>
   <header>
@@ -8,6 +12,20 @@
       <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
+
+  <p>
+    fullPath: <code>{{ route.fullPath }}</code>
+    <br />
+    path: <code>{{ route.path }}</code>
+    <br />
+    query: <code>{{ route.query }}</code>
+    <br />
+    hash: <code>{{ route.hash }}</code>
+    <br />
+    params: <code>{{ route.params }}</code>
+    <br />
+    meta: <code>{{ route.meta }}</code>
+  </p>
 
   <RouterView />
 </template>
