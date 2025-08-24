a3ce3edc850c482c84bd90372be732feb24cc3ae
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-24 17:41:13 +0200

    build: add size check

---
+++ packages/router/size-checks/webRouter_experimental.js
@@ -0,0 +1,7 @@
+export { createWebHistory } from '../dist/vue-router.mjs'
+export {
+  createFixedResolver,
+  experimental_createRouter,
+  normalizeRouteRecord,
+  MatcherPatternPathStatic,
+} from '../dist/experimental/index.mjs'
