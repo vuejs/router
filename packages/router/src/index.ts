f2f95eaf34723970578b3c4ced7004a13e06ed1b
Eduardo San Martin Morote <posva13@gmail.com>
2025-09-18 17:24:18 +0200

    chore: remove the need for tail
    improve build compat for non unix

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * The official Router for Vue 3.
+ *
+ * @packageDocumentation
+ */
+
 export { createWebHistory } from './history/html5'
 export { createMemoryHistory } from './history/memory'
 export { createWebHashHistory } from './history/hash'
@@ -167,10 +173,82 @@ export type { TypesConfig } from './config'
 
 export * from './useApi'
 
-export * from './globalExtensions'
+// Global extensions for Vue
+import type { TypesConfig } from './config'
+import type { Router } from './router'
+import type { RouterLink } from './RouterLink'
+import type { RouterView } from './RouterView'
+import type {
+  NavigationGuard,
+  NavigationGuardWithThis,
+  RouteLocationNormalizedLoaded,
+} from './typed-routes'
 
-/**
- * The official Router for Vue 3.
- *
- * @packageDocumentation
- */
+declare module 'vue' {
+  export interface ComponentCustomOptions {
+    /**
+     * Guard called when the router is navigating to the route that is rendering
+     * this component from a different route. Differently from `beforeRouteUpdate`
+     * and `beforeRouteLeave`, `beforeRouteEnter` does not have access to the
+     * component instance through `this` because it triggers before the component
+     * is even mounted.
+     *
+     * @param to - RouteLocationRaw we are navigating to
+     * @param from - RouteLocationRaw we are navigating from
+     * @param next - function to validate, cancel or modify (by redirecting) the
+     * navigation
+     */
+    beforeRouteEnter?: TypesConfig extends Record<'beforeRouteEnter', infer T>
+      ? T
+      : NavigationGuardWithThis<undefined>
+
+    /**
+     * Guard called whenever the route that renders this component has changed, but
+     * it is reused for the new route. This allows you to guard for changes in
+     * params, the query or the hash.
+     *
+     * @param to - RouteLocationRaw we are navigating to
+     * @param from - RouteLocationRaw we are navigating from
+     * @param next - function to validate, cancel or modify (by redirecting) the
+     * navigation
+     */
+    beforeRouteUpdate?: TypesConfig extends Record<'beforeRouteUpdate', infer T>
+      ? T
+      : NavigationGuard
+
+    /**
+     * Guard called when the router is navigating away from the current route that
+     * is rendering this component.
+     *
+     * @param to - RouteLocationRaw we are navigating to
+     * @param from - RouteLocationRaw we are navigating from
+     * @param next - function to validate, cancel or modify (by redirecting) the
+     * navigation
+     */
+    beforeRouteLeave?: TypesConfig extends Record<'beforeRouteLeave', infer T>
+      ? T
+      : NavigationGuard
+  }
+
+  export interface ComponentCustomProperties {
+    /**
+     * Normalized current location. See {@link RouteLocationNormalizedLoaded}.
+     */
+    $route: TypesConfig extends Record<'$route', infer T>
+      ? T
+      : RouteLocationNormalizedLoaded
+    /**
+     * {@link Router} instance used by the application.
+     */
+    $router: TypesConfig extends Record<'$router', infer T> ? T : Router
+  }
+
+  export interface GlobalComponents {
+    RouterView: TypesConfig extends Record<'RouterView', infer T>
+      ? T
+      : typeof RouterView
+    RouterLink: TypesConfig extends Record<'RouterLink', infer T>
+      ? T
+      : typeof RouterLink
+  }
+}
