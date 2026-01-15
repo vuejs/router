09b65bc8fb32bb40eecef447f8bc7a1562bbec62
Eduardo San Martin Morote <posva13@gmail.com>
2026-01-15 14:15:38 +0100

    chore: update playground

--- packages/playground-file-based/src/main.ts
+++ packages/playground-file-based/src/main.ts
@@ -2,20 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from './router'
-import type {
-  RouteLocationMatched,
-  RouteLocationNormalizedGeneric,
-} from 'vue-router'
-// import { createFixedResolver } from 'vue-router/experimental'
 
 const app = createApp(App)
 app.use(router)
-
-const a = {} as RouteLocationNormalizedGeneric
-const b = {} as RouteLocationMatched
-
-// createFixedResolver([])
-a.matched[0]?.children.length
-b.children?.[0]?.length
 
 app.mount('#app')
