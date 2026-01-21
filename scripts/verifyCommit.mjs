06e16584b945f189bdf580cefa4317dd1a0d4af2
Eduardo San Martin Morote <posva13@gmail.com>
2026-01-21 23:05:46 +0100

    chore: fix verify script

--- scripts/verifyCommit.mjs
+++ scripts/verifyCommit.mjs
@@ -8,7 +8,7 @@ const msgPath = path.resolve('.git/COMMIT_EDITMSG')
 const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const commitRE =
-  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+  /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?!?: .{1,50}/
 
 if (!commitRE.test(msg)) {
   console.log()
