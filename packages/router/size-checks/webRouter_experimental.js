f6923a5def280172f4bd91a6e31e4bbee2a79345
Eduardo San Martin Morote <posva13@gmail.com>
2026-03-10 14:13:02 +0100

    build: include link and view in size computation

--- packages/router/size-checks/webRouter_experimental.js
+++ packages/router/size-checks/webRouter_experimental.js
@@ -1,5 +1,10 @@
 export { createWebHistory } from '../dist/vue-router.js'
 export {
+  //
+  RouterLink,
+  RouterView,
+} from '../dist/vue-router.js'
+export {
   createFixedResolver,
   experimental_createRouter,
   normalizeRouteRecord,
