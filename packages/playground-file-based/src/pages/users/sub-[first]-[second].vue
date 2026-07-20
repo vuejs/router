4c3fe467028794143d3eec7e397e7f23f910c52e
Anoesj <anoesjsadraee@gmail.com>
2026-07-20 16:54:02 +0200

    feat(volar): narrow `typeof useRoute` in a type context
    Handle `useRoute` in type positions (`ReturnType<typeof useRoute>`) the same as `useRoute()` calls.

--- packages/playground-file-based/src/pages/users/sub-[first]-[second].vue
+++ packages/playground-file-based/src/pages/users/sub-[first]-[second].vue
@@ -1,5 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+type Route = typeof route
+type RouteFromType = ReturnType<typeof useRoute>
+type RouteOther = ReturnType<typeof useRoute<'/about'>>
+
+route.params.first satisfies RouteFromType['params']['first']
+route.params.second satisfies RouteFromType['params']['second']
+</script>
 
 <template>
   <h1>{{ String($route.name) }} - {{ $route.path }}</h1>
+  <p>{{ $route.params.first satisfies RouteFromType['params']['first'] }}</p>
+  <p>{{ $route.params.second satisfies RouteFromType['params']['second'] }}</p>
 </template>
