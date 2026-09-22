2d1a0529fca5e0cb93b9a7be3d9e32837d867fbe
Eduardo San Martin Morote <posva13@gmail.com>
2026-09-22 17:41:11 +0200

    feat: expose experimental scroll restoration APIs

--- packages/router/e2e/scroll-restoration/index.ts
+++ packages/router/e2e/scroll-restoration/index.ts
@@ -1,9 +1,6 @@
 import type {
   RouteComponent,
   RouteLocationNormalized,
-  RouteMap,
-  Router,
-  RouteRecordName,
   RouterScrollBehavior,
 } from 'vue-router'
 import {
@@ -12,127 +9,11 @@ import {
   START_LOCATION,
   useRouter,
 } from 'vue-router'
-import type {
-  ComponentInternalInstance,
-  FunctionPlugin,
-  InjectionKey,
-} from 'vue'
+import { createApp, defineComponent, onMounted, onUnmounted, ref } from 'vue'
 import {
-  createApp,
-  defineComponent,
-  getCurrentInstance,
-  inject,
-  onMounted,
-  onScopeDispose,
-  onUnmounted,
-  onUpdated,
-  Plugin,
-  ref,
-} from 'vue'
-import type { EXPERIMENTAL_Router } from 'vue-router/experimental'
-
-interface ScrollRestorationPluginOptions {
-  router: EXPERIMENTAL_Router | Router
-}
-
-window.history.scrollRestoration = 'manual'
-
-const HAS_PENDING_SCROLL_RESTORATION: InjectionKey<
-  Map<string, Set<ComponentInternalInstance>>
-> = Symbol(/* 'HAS_PENDING_SCROLL_RESTORATION' */)
-
-const ScrollRestoration: FunctionPlugin<
-  [options: ScrollRestorationPluginOptions]
-> = (app, { router }) => {
-  const activatedInstances = new Map<string, Set<ComponentInternalInstance>>()
-  app.provide(HAS_PENDING_SCROLL_RESTORATION, activatedInstances)
-
-  const removeAfterEach = router.afterEach((to, from) => {
-    console.log('🚗 After each', from.fullPath, '->', to.fullPath)
-    activatedInstances.clear()
-  })
-
-  app.onUnmount(removeAfterEach)
-}
-
-export const USE_SCROLL_RESTORATION_DEFAULTS = {
-  key: (to: RouteLocationNormalized) => to.path + to.hash,
-} satisfies UseScrollRestorationOptions
-
-/**
- * Options for `useScrollRestoration()`.
- *
- * @see {@link useScrollRestoration}
- */
-export interface UseScrollRestorationOptions<
-  Name extends keyof RouteMap = keyof RouteMap,
-> {
-  /**
-   * A key, derived from the route, that is used to store the scroll position.
-   * By default, the key is the route's path + hash, meaning going from
-   * `/search?q=shoes` to `/search?q=shirts` will reuse the scroll position,
-   * but going from `/search?q=shoes` to `/search?q=shoes#anchor` will not.
-   * Nested `useScrollRestoration()` calls with the same key will **take over**
-   * ancestor calls and completely **override** their scroll restoration
-   * behavior.
-   */
-  key?: string | ((to: RouteLocationNormalized<Name>) => string)
-
-  /**
-   * If true, the user must call the returned `scroll()` to trigger the scroll
-   * restoration. Useful when the displayed content is not displayed immediately
-   * after the navigation, for example when using an animation or a virtualized
-   * list.
-   */
-  manual?: boolean
-}
-
-function useScrollRestoration<Name extends keyof RouteMap = keyof RouteMap>(
-  options?: UseScrollRestorationOptions<Name>
-) {
-  const router = useRouter()
-  const instance = getCurrentInstance()!
-  if (__DEV__ && !instance) {
-    throw new Error(
-      'TODO: useScrollRestoration() must be called in setup() of a component'
-    )
-  }
-  const scrollActivationMap = inject(HAS_PENDING_SCROLL_RESTORATION)!
-
-  const key = options?.key ?? USE_SCROLL_RESTORATION_DEFAULTS.key
-
-  function triggerScrollRestoration() {
-    const keyValue =
-      typeof key === 'function' ? key(router.currentRoute.value) : key
-    let activatedInstances = scrollActivationMap.get(keyValue)
-    if (!activatedInstances) {
-      scrollActivationMap.set(keyValue, (activatedInstances = new Set()))
-    }
-    if (!activatedInstances.has(instance)) {
-      console.log(
-        '♻️ Restoring scroll position for',
-        router.currentRoute.value.fullPath
-      )
-      activatedInstances.add(instance)
-    }
-  }
-
-  // ensure that we trigger when the component is reused or not
-  onMounted(triggerScrollRestoration)
-  onUpdated(triggerScrollRestoration)
-
-  return {
-    scroll: triggerScrollRestoration,
-  }
-}
-
-function _usage() {
-  // no scroll? but where, probably not, let the navigation disable scrolling
-  useScrollRestoration(false)
-
-  // for virtualized lists
-  const scrollEntry = useScrollRestoration()
-}
+  ScrollRestoration,
+  useScrollRestoration,
+} from 'vue-router/experimental'
 
 const Home: RouteComponent = { template: '<div class="home">home</div>' }
 const Foo: RouteComponent = { template: '<div class="foo">foo</div>' }
